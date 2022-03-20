b0231d3466071a04185554f0c76ef9044ec43dbf
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-03-21 01:56:13 +0700

    fix: user display ids

--- services/src/models/user.rs
+++ services/src/models/user.rs
@@ -39,9 +39,9 @@ pub struct UserDisplay {
     pub display_name: String,
     pub email: Option<String>,
     pub avatar_url: Option<String>,
-    pub github_url: Option<String>,
-    pub google_url: Option<String>,
-    pub facebook_url: Option<String>,
+    pub github_id: Option<String>,
+    pub google_id: Option<String>,
+    pub facebook_id: Option<String>,
 }
 
 #[derive(Deserialize, Serialize, AsChangeset, Clone, Debug)]
@@ -58,15 +58,14 @@ pub struct UserUpdate {
 
 impl User {
     pub fn to_user_display(&self) -> UserDisplay {
-        // TODO: get user link from secret keys
         UserDisplay {
             id: self.id.to_string(),
             display_name: self.display_name.clone(),
             email: self.email.clone(),
             avatar_url: self.avatar_url.clone(),
-            github_url: self.google_id.clone(),
-            google_url: self.google_id.clone(),
-            facebook_url: self.facebook_id.clone(),
+            github_id: self.github_id.clone(),
+            google_id: self.google_id.clone(),
+            facebook_id: self.facebook_id.clone(),
         }
     }
 }
