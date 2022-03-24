b892a1da6cc9f54f2a40a49808cc830abd5a53dd
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-03-25 01:05:38 +0700

    fix: url serde owner with Option<String>

--- services/src/models/url.rs
+++ services/src/models/url.rs
@@ -9,14 +9,17 @@ pub struct Url {
     pub id: i64,
     pub code: String,
     pub url: String,
+    #[serde(with = "string_opt")]
     pub owner: Option<i64>,
 }
 
 #[derive(Serialize, Deserialize, Queryable, AsChangeset, Debug)]
 pub struct UrlUpdate {
+    #[serde(with = "string_opt")]
     pub id: Option<i64>,
     pub code: Option<String>,
     pub url: Option<String>,
+    #[serde(with = "string_opt")]
     pub owner: Option<i64>,
 }
 
@@ -43,5 +46,47 @@ mod string {
         String::deserialize(deserializer)?
             .parse()
             .map_err(de::Error::custom)
+    }
+}
+
+mod string_opt {
+    use std::fmt::Display;
+    use std::str::FromStr;
+
+    use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+
+    pub fn serialize<T, S>(
+        value: &Option<T>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        T: Display,
+        S: Serializer,
+    {
+        let val = match value {
+            None => None,
+            Some(s) => Some(s.to_string()),
+        };
+        Option::<String>::serialize(&val, serializer)
+    }
+
+    pub fn deserialize<'de, T, D>(
+        deserializer: D,
+    ) -> Result<Option<T>, D::Error>
+    where
+        T: FromStr,
+        T::Err: Display,
+        D: Deserializer<'de>,
+    {
+        match Option::<String>::deserialize(deserializer) {
+            Err(err) => Err(err),
+            Ok(opt) => match opt {
+                None => Ok(None),
+                Some(s) => match s.parse() {
+                    Ok(val) => Ok(Some(val)),
+                    Err(err) => Err(de::Error::custom(err)),
+                },
+            },
+        }
     }
 }
