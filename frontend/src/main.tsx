4464651bf180d6e1ec25a0ba4f9b0f841a7bad1c
Sherly1001 <longlepkd2000@gmail.com>
2023-01-14 00:34:53 +0700

    fix: hashrouter env

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { hash_router } from './utils/const'
+
+const Router = hash_router ? HashRouter : BrowserRouter
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- frontend/src/utils/const.ts
+++ frontend/src/utils/const.ts
@@ -1,3 +1,4 @@
+export const hash_router = import.meta.env.VITE_HASH_ROUTER == 'true'
 export const api_url = import.meta.env.VITE_API_URL ?? 'https://localhost:3000'
 export const go_url = import.meta.env.VITE_GO_URL
 
